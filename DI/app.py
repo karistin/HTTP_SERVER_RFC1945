c413aa0dbea56f47ba87768f4842c5ca799b0507
karistin <seongjung1109@gmail.com>
2022-07-03 00:16:17 +0900

    ADD : Compelete DI project

--- DI/app.py
+++ DI/app.py
@@ -1,10 +1,11 @@
 from soccerplayer import Soccerplayer
-# from nikeball import Nikeball
+from nikeball import Nikeball
 # from adidasball import Adidasball
 from adidaskorea import AdidasKorea
-from bean import get_bean
+from bean import get_bean, initbean
 
 
 if __name__ == "__main__":
+    initbean()
     print(get_bean(Soccerplayer).ball.get())
     
--- DI/bean.py
+++ DI/bean.py
@@ -2,6 +2,11 @@ from typing import get_type_hints
 
 providers = {}
 
+# 모든 해당 클래스의 맴버 (변수, 클래스 타입) 저장 
+get_type_value ={}
+
+# 모든 class 저장
+get_base = []
 
 def bean(cls):
     obj = cls()
@@ -10,18 +15,25 @@ def bean(cls):
 
 
 def get_bean(cls):
-    if get_type_hints(providers[cls]):
-        for key, value in get_type_hints(providers[cls]).items():  
-            for provider in providers.keys():
-                base = []
-                while True:
-                    if type(provider) == type(object):
-                        break
-                    base.append(provider)
-                    provider = provider.__base__
-                for tem in base:
-                    if value == tem.__base__:
-                        setattr(providers[cls], key, providers[tem])                      
+    if get_type_value[cls]:
+        for get in get_type_value[cls]:
+            for key, value in get.items():
+                for base in get_base:
+                    if value == base.__base__:
+                        setattr(providers[cls], key, providers[base])
+                        break                            
     return providers[cls]
 
 
+def initbean():
+    for provider in providers.keys():
+        get_type_value[provider] = [get_type_hints(provider)]
+        while True:
+            if type(provider) == type(object):
+                break
+            if provider in get_base:
+                break
+            get_base.append(provider)
+            provider = provider.__base__
+
+    
